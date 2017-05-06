dbb33c27fab1226ea912c1d27fe859557634ec74
Audax Anchirinah <audax338@gmail.com>
2017-05-06 22:38:02 +0100

    ES 6 code refactoring

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
-var express = require('express');
-
+import express from 'express';
 // Create our app
-var app = express();
+const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(function (req, res, next){
+app.use((req, res, next)=>{
   if (req.headers['x-forwarded-proto'] === 'https') {
     res.redirect('http://' + req.hostname + req.url);
   } else {
@@ -14,6 +13,6 @@ app.use(function (req, res, next){
 
 app.use(express.static('public'));
 
-app.listen(PORT, function () {
+app.listen(PORT, ()=> {
   console.log('Express server is up on port ' + PORT);
 });
