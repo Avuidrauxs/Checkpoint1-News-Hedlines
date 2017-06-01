151a1a9d1694c292404f1c2f9b57fd669135c171
Audax Anchirinah <audax338@gmail.com>
2017-06-01 17:53:55 +0100

    feat(tests):add more test coverage [delivers]

--- app/tests/components/Login.test.jsx
+++ app/tests/components/Login.test.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import renderer from 'react-test-renderer';
 import expects from 'expect';
-import { shallow } from 'enzyme'
+import { shallow, mount } from 'enzyme';
+import sinon from 'sinon';
+import createHistory from 'history/createBrowserHistory';
 import localStorage from '../__mock__/localStorageMock'
 
 import Login from '../../components/Login';
@@ -22,5 +24,21 @@ describe('Login', () => {
   }
      const login = new Login();
      expect(login.saveGoogleCredentials).toBeInstanceOf(Function);
-   })
+   });
+  //  it('should call the login method when button clicked',()=>{
+  //    const wrapper = shallow(<Login />);
+  //    const mockResponse = {
+  //      profileObj(){
+  //        return {
+  //          givenName: 'audax',
+  //          email: 'audax@fap.com'
+  //        }
+  //      }
+  //    }
+   //
+  //    const spy = sinon.stub(location, 'reload');
+  //    wrapper.instance().saveGoogleCredentials(mockResponse);
+  //    expect(spy.calledOnce).toBe(true);
+   //
+  //  })
 });
--- app/tests/components/Nav.test.jsx
+++ app/tests/components/Nav.test.jsx
@@ -2,26 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import renderer from 'react-test-renderer';
 import expects from 'expect';
+import sinon from 'sinon';
 import { shallow, mount } from 'enzyme';
+import createHistory from 'history/createBrowserHistory';
 
 
 import Nav from '../../components/Nav';
-
+const userProfile = {
+  googleId: "106092711356966952734",
+  imageUrl: "https://lh6.googleusercontent.com/-V-2u0o-CEFI/AAAAAAAAAAI/AAAAAAAABHo/QQ-SVzoEiag/s96-c/photo.jpg",
+  email: "audax338@gmail.com",
+  name: "Audax Anchirinah",
+  givenName: "Audax"
+};
+localStorage.setItem('profile', JSON.stringify(userProfile));
 describe('Nav', () => {
   it('should exist', () => {
     expects(Nav).toExist();
   });
-  it('renders without crashing', () => {
-    const userProfile = {
-      googleId: "106092711356966952734",
-      imageUrl: "https://lh6.googleusercontent.com/-V-2u0o-CEFI/AAAAAAAAAAI/AAAAAAAABHo/QQ-SVzoEiag/s96-c/photo.jpg",
-      email: "audax338@gmail.com",
-      name: "Audax Anchirinah",
-      givenName: "Audax"
-    };
-    localStorage.setItem('profile', JSON.stringify(userProfile));
-       const div = shallow(<Nav/>);
-   });
    it('should render a snapshot',() => {
      const tree = renderer.create(<Nav />).toJSON();
      expect(tree).toMatchSnapshot();
@@ -35,5 +33,35 @@ describe('Nav', () => {
     //  button.simulate('click');
     //  expect(handleFavouriteBtn).toBeCalled()
 
+   });
+  //  it('should call the logout method when button clicked',()=>{
+  //    const wrapper = shallow(<Nav />);
+  //    const userProfile = {
+  //      googleId: "106092711356966952734",
+  //      imageUrl: "https://lh6.googleusercontent.com/-V-2u0o-CEFI/AAAAAAAAAAI/AAAAAAAABHo/QQ-SVzoEiag/s96-c/photo.jpg",
+  //      email: "audax338@gmail.com",
+  //      name: "Audax Anchirinah",
+  //      givenName: "Audax"
+  //    };
+  //    localStorage.setItem('profile', JSON.stringify(userProfile));
+  //    const history = createHistory({ forceRefresh: true });
+  //    const spy = sinon.spy(history, 'push');
+  //    wrapper.instance().logOut();
+  //    expect(spy.calledOnce).toBe(true);
+   //
+  //  });
+   it('should not call the handleFavouriteBtn Nav is mounted',()=>{
+     const userProfile = {
+       googleId: "106092711356966952734",
+       imageUrl: "https://lh6.googleusercontent.com/-V-2u0o-CEFI/AAAAAAAAAAI/AAAAAAAABHo/QQ-SVzoEiag/s96-c/photo.jpg",
+       email: "audax338@gmail.com",
+       name: "Audax Anchirinah",
+       givenName: "Audax"
+     };
+     localStorage.setItem('profile', JSON.stringify(userProfile));
+     const wrapper = mount(<Nav />);
+     const spy = sinon.spy(Nav.prototype, 'handleFavouriteBtn');
+     wrapper.instance().handleFavouriteBtn();
+     expect(spy.callCount).toBe(0);
    });
 });
